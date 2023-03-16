0d33f016666067e57dbed854718fb5d31c99653f
Cat Malum <cat-malum@yandex.ru>
2023-03-16 19:59:25 +0300

    Creted getters

--- demo/src/stores/tasks.js
+++ demo/src/stores/tasks.js
@@ -1,9 +1,53 @@
 import { defineStore } from 'pinia';
+import { useFiltersStore } from './filters';
+import { useUsersStore } from '@/stores/users';
 
 export const useTasksStore = defineStore('tasks', {
     state: () => ({
         tasks: [],
     }),
-	getters: {},
+	getters: {
+        filteredTasks: state => {
+            const filtersStore = useFiltersStore();
+            const filtersAreEmpty = Object.values(filtersStore.filters)
+                .every(value => !value.length);
+
+            if (filtersAreEmpty) {
+                return state.tasks;
+            }
+
+            const searchFilter = task => task.title
+                .toLowerCase()
+                .includes(filtersStore.filters.search.toLowerCase().trim());
+
+            const usersFilter = task => filtersStore.filters.users
+                .some(userId => userId === task.userId);
+            
+            const statusesFilter = task => filtersStore.filters.statuses
+                .some(el => el === task.status || el === task.timeStatus);
+            
+            return state.tasks.filter(task => {
+                let result = {
+                    search: searchFilter,
+                    users: usersFilter,
+                    statuses: statusesFilter,
+                };
+
+                return Objext.entries(result)
+                    .every(([key, callback]) =>
+                        !filtersStore.filters[key].length || callback(task));
+            });
+        },
+        getTaskUserById: () => id => {
+            const usersStore = useUsersStore();
+
+            return usersStore.users.find(user => user.id === id);
+        },
+        sidebarTasks: state => {
+            return state.filteredTasks
+                .filter(task => !task.columnId)
+                .sort((a, b) => a.sortOrder - b.sortOrder);
+        },
+    },
 	actions: {},
 });
